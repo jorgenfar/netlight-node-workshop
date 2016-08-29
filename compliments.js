bf35bd3819ca8fc8a734cf896be7d610718f51f0
Jørgen Faret <jorgen.faret@netlight.com>
2016-08-29 19:37:13 +0200

    Add step 3 in README

--- compliments.js
+++ compliments.js
@@ -1,7 +1,14 @@
 const compliments = require('./compliments.json');
 
-function random() {
-  return compliments[Math.floor(Math.random()*compliments.length)];
+function random(min, max) {
+  return min + Math.floor(Math.random() * max)];
+}
+
+function get(index) {
+  if (index < 0 || index > compliments.length -1) {
+    throw new Error('Bad index provided to get()');
+  }
+  return compliments[random(0, compliments.length-1)];
 }
 
 module.exports = {
